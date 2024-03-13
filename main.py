cdaf1b91b1336dfc49332e0995a90e0478bcbf96
A_Agboola18 <A_Agboola18@cranford.hounslow.sch.uk>
2024-03-13 11:33:06 +0000

    added print added converter code

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+import PySimpleGUI as sg
+
+
+ftinch_label = sg.Text("enter ft and inch:  ")
+ftinch_input_box = sg.InputText(key ="ftinch")
+
+
+m_label = sg.Text("Value in metres : ")
+m_input_box = sg.InputText(key = "metres")
+
+converter_btn = sg.Button("Convert")
+
+
+window = sg.Window(title="Feet and inches to metres converter.")
